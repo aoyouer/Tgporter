8ba05e5aeb4ba2ed27c6e1bb7d9c516e0ffa1bf8
root <root@kvmvps732-7aqy7.olink.cloud>
2021-10-22 09:12:03 -0400

    base

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from telethon import TelegramClient, events, sync
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.errors import UserAlreadyParticipantError
 from datetime import datetime, tzinfo
 import pytz
 import telethon
@@ -6,7 +9,7 @@ import telethon
 utc = pytz.UTC
 
 # 从配置文件中读取信息
-api_id = 
+api_id =
 api_hash = ''
 client = TelegramClient('tgporter', api_id, api_hash)
 recorded_grouped_id = None
@@ -25,14 +28,42 @@ async def send_files(target_id):
     message_list = []
     recorded_grouped_id = None
 
+# 从会话列表中进行选择
+async def select_dialog():
+    channels = []
+    async for d in client.iter_dialogs():
+        if d.is_channel:
+            channels.append(d)
+    print("------频道列表-----")
+    for index in range(len(channels)):
+        print(str(index)+"."+channels[index].name)
+    select_index = int(input("请输入频道的索引: "))
+    # print(channels[select_index])
+    return channels[select_index].entity
 
 async def main():
     global recorded_grouped_id, recorded_caption, message_list
     await client.start()
-    source_id = input("请输入消息源频道id: ")
-    target_id = input("请输入消息目标频道id: ")
-    limit = int(input("采集数量上限(输入0不设限): "))
-    if limit == 0:
+    source_select_mode = input("请输入源频道选择方式:\n1.输入id 2.从已加入的频道中选择\n")
+    if source_select_mode == "2":
+        input_channel = await select_dialog()
+    else:
+        source_type = input("请输入源频道类型\n1.公共 2.私有\n")
+        input_channel = input("请输入源频道id: ")
+        if source_type == "2":
+            try:
+                s_updates = await client(ImportChatInviteRequest(input_channel))
+            except UserAlreadyParticipantError:
+                print("已加入私有频道")
+            print(s_updates)
+            input_channel = s_updates.chats[0]
+
+    input("请从列表中选择目的频道 回车继续")
+    output_channel = await select_dialog()
+
+
+    limit = int(input("搬运数量上限(输入0不设限): "))
+    if limit == 0 or limit == None:
         limit = None
     # interval = int(input("请求间隔"))
     time_select = input("是否指定时间范围\n1.是 2.否\n")
@@ -45,25 +76,28 @@ async def main():
         end_date = datetime.strptime(
             end_date_str, "%Y-%m-%d").replace(tzinfo=utc)
         print("采集", start_date, "至", end_date, "范围内的内容")
-        messages = client.iter_messages(
-            source_id, reverse=True, limit=limit, offset_date=start_date)
+        messages = client.iter_messages(input_channel, reverse=True, limit=limit, offset_date=start_date)
+
     else:
-        messages = client.iter_messages(source_id, reverse=True, limit=limit)
+        messages = client.iter_messages(input_channel, reverse=True, limit=limit)
+
 
     async for message in messages:
         # 循环中对消息时间进行判断
-        if message.date.replace(tzinfo=utc) > end_date.replace(tzinfo=utc):
-            print("时间超出，结束采集")
-            break
+        if time_select == "1":
+            if message.date > end_date:
+                print("时间超出，结束采集")
+                break
+
         if isinstance(message, telethon.tl.patched.Message):
             # 针对合并发送的文件(如相册)要特殊处理
             if message.grouped_id == None:
                 # 普通消息直接转发
                 # 如果前面还有文件则先发送文件
                 if len(message_list) != 0:
-                    await send_files(target_id)
+                    await send_files(output_channel)
                 print("普通消息转发 消息日期:", message.date)
-                await client.send_message(target_id, message)
+                await client.send_message(output_channel, message)
             else:
                 # recorded_group_id 初始化
                 if recorded_grouped_id == None:
@@ -77,12 +111,16 @@ async def main():
                     message_list.append(message)
                 else:
                     # 分组发生变化，先发送文件
-                    await send_files(target_id)
+                    await send_files(output_channel)
         # else:
             # print(message)
     if recorded_grouped_id != None:
-        await send_files(target_id)
-
+        await send_files(output_channel)
+    # 退出私有频道
+    # if source_type == "2":
+    #     await client(LeaveChannelRequest(input_channel))
+    # if  target_type == "2":
+    #     await client(LeaveChannelRequest(output_channel))
 
 with client:
     client.loop.run_until_complete(main())
